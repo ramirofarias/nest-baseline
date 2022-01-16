5dc631cae07d7b2559bc5e7b052a203c59eeba2b
Ramiro Farias <ramiro.farias@estoes.me>
2022-01-15 21:11:04 -0300

    Refactored pagination without qb

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/user.entity";
-import { createQueryBuilder, Repository } from "typeorm";
+import {
+  createQueryBuilder,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThanOrEqual,
+  Repository,
+} from "typeorm";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { PaginatedReportsDto } from "./dtos/paginated-reports.dto";
 import { Report } from "./report.entity";
@@ -18,39 +25,36 @@ export class ReportsService {
   }
 
   public async paginated(query: PaginatedReportsDto) {
-    const reports = await this.repo.createQueryBuilder("report");
+    const filters: any = {};
+    const order: any = {};
     const pageSize = query.pageSize || 10;
     const page = Number(query.page) || 1;
     const skip = (page - 1) * pageSize;
 
     if (query.searchQuery) {
-      reports.where("report.make like :make", {
-        make: `%${query.searchQuery}%`,
-      });
+      filters.make = Like(`%${query.searchQuery}%`);
     }
 
     if (query.before) {
-      reports.andWhere("report.year < :year", {
-        year: query.before,
-      });
+      filters.year = LessThanOrEqual(query.before);
     }
 
     if (query.after) {
-      reports.andWhere("report.year > :year", {
-        year: query.after,
-      });
-    }
-    if (query.orderBy) {
-      reports.orderBy(query.orderBy, query.orderType, "NULLS LAST");
+      filters.year = MoreThanOrEqual(query.after);
     }
 
-    const [data, total] = await reports
-      .skip(skip)
-      .take(pageSize)
-      .leftJoinAndSelect("report.user", "user")
-      .getManyAndCount();
+    if (query.orderBy) {
+      order[query.orderBy] = query.orderType;
+    }
+    const [results, total] = await this.repo.findAndCount({
+      take: pageSize,
+      skip: skip,
+      where: filters,
+      order: order,
+    });
+
     return {
-      data: data,
+      data: results,
       total: total,
       page: page,
       last_page: Math.ceil(total / pageSize),
@@ -63,6 +67,46 @@ export class ReportsService {
     return this.repo.save(report);
   }
 }
+
+//   if (query.searchQuery) {
+//     reports.where("report.make like :make", {
+//       make: `%${query.searchQuery}%`,
+//     });
+//   }
+
+//   if (query.before) {
+//     reports.andWhere("report.year < :year", {
+//       year: query.before,
+//     });
+//   }
+
+//   if (query.after) {
+//     reports.andWhere("report.year > :year", {
+//       year: query.after,
+//     });
+//   }
+//   if (query.orderBy) {
+//     reports.orderBy(query.orderBy, query.orderType, "NULLS LAST");
+//   }
+
+//   const [data, total] = await reports
+//     .skip(skip)
+//     .take(pageSize)
+//     .leftJoinAndSelect("report.user", "user")
+//     .getManyAndCount();
+//   return {
+//     data: data,
+//     total: total,
+//     page: page,
+//     last_page: Math.ceil(total / pageSize),
+//   };
+// }
+
+// create(reportDto: CreateReportDto, user: User) {
+//   const report = this.repo.create(reportDto);
+//   report.user = user;
+//   return this.repo.save(report);
+// }
 
 // const [results, total] = await this.repo.findAndCount({
 //   take: pageSize,
